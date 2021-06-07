d49e73067925c71493835a876eca0cf1ae3c13cf
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-07 13:48:07 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        animal.AnimalName = "Blackbirds";
+
+        Animal Bird = new Bird();
+        Bird.AnimalName = "Chickadees";
+
+        Bird bird = new Bird();
+        bird.AnimalName = "Crows";
+        bird.BirdType = "American Crow";
+    }
+}
